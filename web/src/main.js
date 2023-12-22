d53aff4464a7ecc12e816e429f900937244e7141
danonishe <99720485+danonishe@users.noreply.github.com>
2023-12-22 19:09:56 +0300

    create vue project

---
+++ web/src/main.js
@@ -0,0 +1,19 @@
+import '@/assets/layout/index.scss';
+import { createApp } from 'vue';
+import App from './App.vue';
+import components from '@/components/UI';
+import router from './router/index';
+import 'bootstrap/dist/css/bootstrap.css';
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
+
+const app = createApp(App);
+
+components.forEach(component=>(
+    app.component(component.name, component)
+))
+
+
+app
+.use(router)
+.use(bootstrap)
+.mount('#app')
