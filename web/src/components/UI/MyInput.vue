81d863126c2be2ebe21fa90926cd1082fac52ebe
Никита <100412266+Niks6455@users.noreply.github.com>
2023-12-22 20:17:12 +0300

    2

--- web/src/components/UI/MyInput.vue
+++ web/src/components/UI/MyInput.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <div className="input">
+    <input placeholder="aaa">
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,9 @@ name: 'my-input',
 </script>
 
 <style>
-
+.input{
+    width: 200px;
+    height: 30px;
+    background-color: aqua;
+}
 </style>
