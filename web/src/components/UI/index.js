949dbc359b64d307933e3d1ad6d641c288080425
danonishe <99720485+danonishe@users.noreply.github.com>
2023-12-23 11:32:14 +0300

    add header component

--- web/src/components/UI/index.js
+++ web/src/components/UI/index.js
@@ -1,9 +1,13 @@
 import FormsAut from "@/components/Forms/FormsAut";
 import MyButton from "@/components/UI/MyButton"
 import FormsReg from "@/components/Forms/FormsReg"
+import MyHeader from "@/components/UI/MyHeader";
+import MyInput from "@/components/UI/MyInput";
 export default [
   
     FormsAut,
     MyButton,
-    FormsReg
+    FormsReg,
+    MyHeader,
+    MyInput
 ];
