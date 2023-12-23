e5d80d7fed990916728ae1b44a0c42f9e482f0d9
Никита <100412266+Niks6455@users.noreply.github.com>
2023-12-23 13:50:47 +0300

    home

---
+++ web/src/components/UI/MyFooter.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer>
+    <div class="footer__text__block">
+        <img src="@/assets/images/footer/mesto.svg">
+        <p>Что рядом?</p>
+    </div>
+    <div class="footer__text__block">
+        <img src="@/assets/images/footer/march.svg">
+        <p>Маршпут</p>
+    </div>
+    <div class="footer__text__block">
+        <img src="@/assets/images/footer/add.svg">
+        <p>Добавить</p>
+    </div>
+    <div class="footer__text__block">
+        <img src="@/assets/images/footer/izbr.svg">
+        <p>Избранное</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'my-footer'
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/layout/index.scss';
+
+footer{
+    position: absolute;
+    bottom: 0;
+    height: 52px;
+    width: 100%;
+    background-color: $colorsFooterBg;
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 16px;
+}
+.footer__text__block{
+    align-items: center;
+    text-align: center;
+}
+.footer__text__block p{
+    color: $colorsTextFooter ;
+    font-size: 12px;
+}
+</style>
--- web/src/pages/HomePage.vue
+++ web/src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-
+    <my-footer/>
   </template>
   
   <script>
