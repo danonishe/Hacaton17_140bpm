980373ed0f678eca0a5504093447fcb4f45873e4
Artur <artur.tonoyan.04@list.ru>
2023-12-24 01:38:33 +0300

    artur7

--- React-App/react__app/src/components/pages/myPage/myPage.js
+++ React-App/react__app/src/components/pages/myPage/myPage.js
@@ -1,4 +1,31 @@
 import React from "react";
-export default function HomePage() {
-  return <main>fff</main>;
+import styles from "./myPage.module.scss";
+export default function MyPage() {
+  return (
+    <main className={styles.MyPage}>
+      <div className={styles.title}>
+        <h3>Личный кабинет</h3>
+      </div>
+      <div className={styles.MyPage_inner}>
+        <div className={styles.foto}>
+          <img
+            className={styles.foto1}
+            width={64}
+            src="./img/map/icon2.png"
+            alt="111"
+          ></img>
+          <img
+            className={styles.foto2}
+            src="./img/map/camera.svg"
+            alt="111"
+          ></img>
+        </div>
+
+        <div>
+          <span className={styles.name}>imaginarium</span>
+          <span>tayka@gmail.com</span>
+        </div>
+      </div>
+    </main>
+  );
 }
