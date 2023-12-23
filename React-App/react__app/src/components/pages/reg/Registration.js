552a6aba111fb33b39bac920c58096c9ab6322e9
Никита <100412266+Niks6455@users.noreply.github.com>
2023-12-23 23:27:32 +0300

    jjj

--- React-App/react__app/src/components/pages/reg/Registration.js
+++ React-App/react__app/src/components/pages/reg/Registration.js
@@ -1,42 +1,75 @@
-import React from 'react';
 import styles from './Registration.module.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Header from '../../ui/Header/Header';
+import React, { useState } from 'react';
+import axios from 'axios';
 export default function Registration() {
+    const [emailValue, setEmailValue] = useState('');
+    const [nameValue, setNameValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+    const [confirmPasswordValue, setConfirmPasswordValue] = useState('');
+    let navigate = useNavigate();
+    function handleRiderect() {
+        navigate("/Interests");
+      }
+    const handleRegistration = () => {
+        // ... (ваша логика обработки регистрации)
+        if (passwordValue !== confirmPasswordValue) {
+            console.log("Пароли не совпадают");
+            return;
+        }
+
+        // Создайте объект с данными для отправки
+        const data = {
+            login: emailValue,
+            name: nameValue,
+            password: passwordValue
+        };
+
+        // Выполните Axis запрос
+        console.log(data);
+        axios.post("https://c33f-95-174-102-182.ngrok-free.app/auth/register", data)
+            .then(response => {
+                console.log("Авторизация прошла успешно");
+                const myToken = response.data.token;
+                console.log(myToken);
+                handleRiderect();
+              
+            })
+            .catch(error => {
+                console.log("нет", error);
+            });
+    };
+   
+
 
     return (
+
+
         <div className={styles.mian}>
-        <Header/>
-      
-        <div class={styles.title}>
+            <Header />
+
+            <div className={styles.title}>
                 <p className='font-impact'>ПЕРЕДВИГАЙТЕСЬ БОЛЬШЕ С 140BPM</p>
             </div>
-        <div className={styles.Block__aut}>
-            <div className={styles.Block__aut__inner}>
-                <div className={styles.input__block}>
-                    <input placeholder="Логин" class="form-control" v-model="loginValue" type="text" />
-                </div>
-                <div className={styles.input__block}>
-                    <input placeholder="Почта" class="form-control" v-model="loginValue" type="text" />
-                </div>
-                <div className={styles.input__block}>
-                    <input placeholder="Имя" class="form-control" v-model="loginValue" type="text" />
-                </div>
-                <div className={styles.input__block}>
-                    <input placeholder="Пароль" class="form-control" v-model="loginValue" type="text" />
-                </div>
-                <div className={styles.input__block}>
-                    <input placeholder="Подтвердите пароль" class="form-control" v-model="passwordValue" type="password" />
-                </div>
-                <div class={styles.block__btn}>
-                    <button type="button" class="btn btn-success">Зарегестрироваться</button>
-                </div>
-                <div className={styles.firstStart}>
-                    <Link className={styles.firstStartText} to={"./../Authorization"}> <p>Уже зарегистрирован</p></Link>
+            <div className={styles.Block__aut}>
+                <div className={styles.Block__aut__inner}>
+                    <div className={styles.input__block}>
+                        <input placeholder="Почта" className="form-control" value={emailValue} onChange={e => setEmailValue(e.target.value)} type="text" />                </div>
+                    <div className={styles.input__block}>
+                        <input placeholder="Имя" className="form-control" value={nameValue} onChange={e => setNameValue(e.target.value)} type="text" />                </div>
+                    <div className={styles.input__block}>
+                        <input placeholder="Пароль" className="form-control" value={passwordValue} onChange={e => setPasswordValue(e.target.value)} type="password" />                </div>
+                    <div className={styles.input__block}>
+                        <input placeholder="Подтвердите пароль" className="form-control" value={confirmPasswordValue} onChange={e => setConfirmPasswordValue(e.target.value)} type="password" />                </div>
+                    <div className={styles.block__btn}>
+                        <button type="button" className="btn btn-success" onClick={handleRegistration}>Зарегистрироваться</button>                </div>
+                    <div className={styles.firstStart}>
+                        <Link className={styles.firstStartText} to={"./../Authorization"}> <p>Уже зарегистрирован</p></Link>
+                    </div>
                 </div>
             </div>
-        </div>
-        <img src="./img/nlo.svg" className={styles.imgFoot} alt='nlo'></img>
+            <img src="./img/nlo.svg" className={styles.imgFoot} alt='nlo'></img>
         </div>
 
     )
