cb3e39427615c8abc618da96142d062c33494beb
Artur <artur.tonoyan.04@list.ru>
2023-12-24 04:50:30 +0300

    geyvod13

--- React-App/react__app/src/components/pages/tinder/Tinder.js
+++ React-App/react__app/src/components/pages/tinder/Tinder.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import styles from "./Tinder.module.scss"
-import Slider from './sviper/sviper';
+import React from "react";
+import styles from "./Tinder.module.scss";
+import Slider from "./sviper/sviper";
 
 export default function Tinder() {
-    return (
-        <div className={styles.TinderMain}>
-            
-            <img className={styles.card__tindBg} src='./img/tinder/bgTind.png'></img>
-            <Slider/>
-      
-        </div >
-
-        
-
-
-    )
-    
+  return (
+    <div className={styles.TinderMain}>
+      <img className={styles.card__tindBg} src="./img/tinder/bgTind.png"></img>
+      <Slider />
+    </div>
+  );
 }
--- React-App/react__app/src/components/pages/tinder/sviper/sviper.js
+++ React-App/react__app/src/components/pages/tinder/sviper/sviper.js
@@ -1,71 +1,92 @@
-import React from 'react';
-import styles from "./sviper.module.scss"
+import React, { useEffect } from "react";
+import styles from "./sviper.module.scss";
 // core version + navigation, pagination modules:
 // import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper/bundle";
 
 // import styles bundle
-import 'swiper/css/bundle';
-import { Navigation, Pagination } from 'swiper/modules';
+import "swiper/css/bundle";
+import { Navigation, Pagination } from "swiper/modules";
 // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Slider() {
-    // init Swiper:
-    const swiper = new Swiper('.swiper', {
-        // configure Swiper to use modules
-        modules: [Navigation, Pagination],
-    })
-    return (
-        <div className="swiper" style={{display: "flex", justifyContent: "center"}}>
+  useEffect(() => {
+    const swiper = new Swiper(".swiper", {
+      modules: [Navigation, Pagination],
+    });
+  }, []);
 
-       
-            <div className="swiper-wrapper" style={{position: "relative",  zIndex:"5",  width:"85%", height:"480px", backgroundColor:"#fff", borderRadius:"12px", marginTop:"45%"}}>
-                <div  className="swiper-slide">
-                    
+  // init Swiper:
 
-                        <div className={styles.card__tind}>
-                            <img className={styles.card__tindimgPlase} src='./img/tinder/faina.jpeg'></img>
-                            <div className={styles.infoCard}>
-                                <p className={styles.title}>Память Фаине!</p>
-                                <p className={styles.Subtitle}>В этом году у уроженки города Таганрога юбилей. В честь этого вечером будет концерт с труппой театра.</p>
-                                <button className="btn btn-primary">Маршрут</button>
-                            </div>
-                        </div>
-                   
-                </div>
-                <div  className="swiper-slide">
-                  
-
-                        <div className={styles.card__tind}>
-                            <img className={styles.card__tindimgPlase} src='./img/tinder/faina.jpeg'></img>
-                            <div className={styles.infoCard}>
-                                <p className={styles.title}>Память Фаине!</p>
-                                <p className={styles.Subtitle}>В этом году у уроженки города Таганрога юбилей. В честь этого вечером будет концерт с труппой театра.</p>
-                                <button className="btn btn-primary">Маршрут</button>
-                            </div>
-                        </div>
-                   
-                </div>
-                <div  className="swiper-slide">
-                    
-                        <div className={styles.card__tind}>
-                            <img className={styles.card__tindimgPlase} src='./img/tinder/faina.jpeg'></img>
-                            <div className={styles.infoCard}>
-                                <p className={styles.title}>Память Фаине!</p>
-                                <p className={styles.Subtitle}>В этом году у уроженки города Таганрога юбилей. В честь этого вечером будет концерт с труппой театра.</p>
-                                <button className="btn btn-primary">Маршрут</button>
-                            </div>
-                        </div>
-                   
-                </div>
+  return (
+    <div
+      className="swiper"
+      style={{ display: "flex", justifyContent: "center" }}
+    >
+      <div
+        className="swiper-wrapper"
+        style={{
+          position: "relative",
+          zIndex: "5",
+          width: "85%",
+          height: "480px",
+          backgroundColor: "#fff",
+          borderRadius: "12px",
+          marginTop: "45%",
+        }}
+      >
+        <div className="swiper-slide">
+          <div className={styles.card__tind}>
+            <img
+              className={styles.card__tindimgPlase}
+              src="./img/tinder/faina.jpeg"
+            ></img>
+            <div className={styles.infoCard}>
+              <p className={styles.title}>Память Фаине!</p>
+              <p className={styles.Subtitle}>
+                В этом году у уроженки города Таганрога юбилей. В честь этого
+                вечером будет концерт с труппой театра.
+              </p>
+              <button className="btn btn-primary">Маршрут</button>
             </div>
+          </div>
         </div>
-
-    )
-
+        <div className="swiper-slide">
+          <div className={styles.card__tind}>
+            <img
+              className={styles.card__tindimgPlase}
+              src="./img/tinder/faina.jpeg"
+            ></img>
+            <div className={styles.infoCard}>
+              <p className={styles.title}>Память Фаине!</p>
+              <p className={styles.Subtitle}>
+                В этом году у уроженки города Таганрога юбилей. В честь этого
+                вечером будет концерт с труппой театра.
+              </p>
+              <button className="btn btn-primary">Маршрут</button>
+            </div>
+          </div>
+        </div>
+        <div className="swiper-slide">
+          <div className={styles.card__tind}>
+            <img
+              className={styles.card__tindimgPlase}
+              src="./img/tinder/faina.jpeg"
+            ></img>
+            <div className={styles.infoCard}>
+              <p className={styles.title}>Память Фаине!</p>
+              <p className={styles.Subtitle}>
+                В этом году у уроженки города Таганрога юбилей. В честь этого
+                вечером будет концерт с труппой театра.
+              </p>
+              <button className="btn btn-primary">Маршрут</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
